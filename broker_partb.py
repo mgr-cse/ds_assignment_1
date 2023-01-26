c3d1fae0c2712b0d5a5ce92ae9fe31114111cdb5
laukikgawai <laukikgawai69@gmail.com>
2023-01-26 14:02:05 +0530

    added database orm

---
+++ broker_partb.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+import threading
+from flask_sqlalchemy import SQLAlchemy
+
+username = 'mattie'
+password = 'password'
+database = 'psqlqueue'
+db_port = '5432'
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{username}:{password}@localhost:{db_port}/{database}"
+
+# # queue database structures
+db = SQLAlchemy(app)
+ 
+class Producer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+    
+class Consumer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+    offset = db.Column(db.Integer, nullable=False)
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    producers = db.relationship('Producer', backref='topic')
+    consumers = db.relationship('Consumer', backref='topic')
+    messages  = db.relationship('Message', backref='topic')
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+    message_content = db.Column(db.String(255))
